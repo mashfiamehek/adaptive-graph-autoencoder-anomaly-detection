bb1459a469038c2ee6541481399531e86452c53d
Mehek <164574067+mashfiamehek@users.noreply.github.com>
2026-07-09 13:27:19 +0530

    Create gcn_autoencoder.py

---
+++ code/models/gcn_autoencoder.py
@@ -0,0 +1,8 @@
+"""
+Graph Autoencoder implementation using Graph Convolutional Networks.
+
+This module implements the encoder and decoder architecture used
+for anomaly detection in Industrial IoT sensor networks.
+
+Author: Mashfia Mehek
+"""
