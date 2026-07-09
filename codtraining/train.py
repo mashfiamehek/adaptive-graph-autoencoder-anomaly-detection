74d6b7d28f44a699ad29de5c718fc8bf41f9c10c
Mehek <164574067+mashfiamehek@users.noreply.github.com>
2026-07-09 13:28:31 +0530

    Create train.py

---
+++ codtraining/train.py
@@ -0,0 +1,12 @@
+"""
+Model training script.
+
+Responsibilities:
+- Load dataset
+- Build graph
+- Train Adaptive Graph Autoencoder
+- Save trained model
+- Generate evaluation metrics
+
+Author: Mashfia Mehek
+"""
