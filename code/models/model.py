15c33b856022d4c851bd0c1ed8d7c1ecbf474c92
Mehek <164574067+mashfiamehek@users.noreply.github.com>
2026-07-09 13:26:45 +0530

    Create model.py

---
+++ code/models/model.py
@@ -0,0 +1,13 @@
+"""
+Adaptive Graph Autoencoder Model
+
+This module contains the implementation of the adaptive graph
+autoencoder architecture for Industrial IoT anomaly detection.
+
+Framework:
+- Graph Convolutional Networks (GCN)
+- LSTM Encoder
+- Graph Autoencoder Decoder
+
+Author: Mashfia Mehek
+"""
